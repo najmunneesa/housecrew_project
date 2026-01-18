578037f2654b8fe3fbc5af7b8f961ef3eb6f0ab3
NAJMUNNEESA <adamnaju@NAJMUNNEESAs-MacBook-Air.local>
2026-01-18 20:29:03 +0530

    configure static and whitenoise

--- housecrew/settings.py
+++ housecrew/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^^_gv_!!#=pn*6lq^382r%3n*5lz%w#_w5b4_8*=&d7@so(-6_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Falsepi
+DEBUG = False
 
-ALLOWED_HOSTS = ['Najmunneesa.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
